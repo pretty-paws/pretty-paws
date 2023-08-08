0c381a0829fd04a0529414545a9a1dacbec45340
AuroraPolaire <sea.forest.mountains@gmail.com>
2023-08-08 15:40:55 +0300

    feature/no-ref/created-global-container-added-global-styles

--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { GlobalStyles } from './global/GlobalStyles';
+import { GlobalContainer } from './global/GlobalContainer';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <GlobalStyles />
+    <GlobalContainer>
+      <App />
+    </GlobalContainer>
   </React.StrictMode>
 );
