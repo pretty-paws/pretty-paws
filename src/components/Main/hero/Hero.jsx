4b7146d3ce48164b44d31eb1f701ae98084cf4fd
Oduvanchik43 <oduvanchikyan@gmail.com>
2023-09-04 19:52:21 +0300

    feat:change logic hero slider. add new styles for hero and category blocks. adaptive block hero

--- src/components/Main/hero/Hero.jsx
+++ src/components/Main/hero/Hero.jsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import imgReptiles from '../../../img/hero/reptiles.jpg';
-import imgBird from '../../../img/hero/bird.jpg';
+import { React, useRef, useState, useEffect } from 'react';
+// Import Swiper React components
+import { Swiper, SwiperSlide } from 'swiper/react';
+// import required modules
+import { Autoplay, Pagination } from 'swiper/modules';
+import * as ReactDOMServer from 'react-dom/server';
+import sprite from '../../../img/svg-sprite/sprite.svg';
+
 import imgCat from '../../../img/hero/cat.png';
 import imgDog from '../../../img/hero/dog.jpg';
 import imgFish from '../../../img/hero/fish.jpg';
 import imgRodents from '../../../img/hero/rodents.jpg';
-import {
-  StyledHero,
-  AnimalsBarContainer,
-  StyleAnimalsBar,
-} from './Hero.styled';
+import imgBird from '../../../img/hero/bird.jpg';
+import imgReptiles from '../../../img/hero/reptiles.jpg';
+
+import CategoryType from './CategoryType';
+
+// Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/pagination';
-import CategoryType from './CategoryType';
-import SwiperCore from 'swiper';
-import { Pagination, Autoplay } from 'swiper/modules';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import sprite from '../../../img/svg-sprite/sprite.svg';
-
-import * as ReactDOMServer from 'react-dom/server';
-// Инициализируем Swiper
-SwiperCore.use([Pagination, Autoplay]);
+import { StyledHero, StyleAnimalsBar } from './Hero.styled';
+import { GlobalContainer } from '../../../global/GlobalContainer';
 
 const animalsData = [
   { index: 1, icon: 'cat', text: 'Коти', img: imgCat },
@@ -30,56 +29,100 @@ const animalsData = [
   { index: 5, icon: 'bird', text: 'Птахи', img: imgBird },
   { index: 6, icon: 'lizard', text: 'Рептилії', img: imgReptiles },
 ];
+
 const Hero = () => {
-  const renderCustomPagination = (swiper, current) => {
+  const [swiper, setSwiper] = useState(null);
+  const paginationRef = useRef(null);
+
+  const renderCustom = (swiper, current) => {
     return ReactDOMServer.renderToStaticMarkup(
       animalsData.map(animal => (
-        <div key={animal.index} onClick={() => swiper.slideTo(animal.index)}>
-          <StyleAnimalsBar isActive={animal.index === current}>
-            <div className="animals-bar-icon-box">
-              <svg className="animals-bar-icon" width="24px" height="24px">
-                <use href={`${sprite}#${animal.icon}`} />
-              </svg>
-            </div>
-            <span className="animals-bar-text">
-              {current === animal.index ? animal.text : ''}
-            </span>
-          </StyleAnimalsBar>
-        </div>
+        <StyleAnimalsBar
+          className="pagination__custom"
+          key={animal.index}
+          onClick={() => handlePaginationItemClick(animal.index)}
+          isActive={animal.index === current}
+        >
+          <div className="animals-bar-icon-box">
+            <svg className="animals-bar-icon" width="24px" height="24px">
+              <use href={`${sprite}#${animal.icon}`} />
+            </svg>
+          </div>
+          <span className="animals-bar-text">
+            {current === animal.index ? animal.text : ''}
+          </span>
+        </StyleAnimalsBar>
       ))
     );
   };
+  console.log(swiper);
+  const handlePaginationItemClick = event => {
+    const clickedElement = event.target;
+    const paginationItems = paginationRef.current.querySelectorAll(
+      '.pagination__custom'
+    );
+    paginationItems.forEach((item, index) => {
+      if (item === clickedElement) {
+        swiper.slideTo(index);
+        console.log('index of slide  = ', index);
+      }
+    });
+  };
 
+  useEffect(() => {
+    if (paginationRef.current && swiper) {
+      paginationRef.current.addEventListener(
+        'click',
+        handlePaginationItemClick
+      );
+    }
+    console.log(swiper);
+    console.log('ff');
+
+    return () => {
+      if (paginationRef.current && swiper) {
+        paginationRef.current.removeEventListener(
+          'click',
+          handlePaginationItemClick
+        );
+      }
+    };
+  }, [swiper]);
   return (
     <StyledHero>
       <Swiper
-        slidesPerView={1}
-        autoplay={{
-          delay: 15000,
-          disableOnInteraction: false,
-        }}
-        speed={1500}
+        onSwiper={swiper => setSwiper(swiper)}
+        allowTouchMove={false}
         pagination={{
-          el: '.swiper-pagination',
-          type: 'custom',
+          el: paginationRef.current,
           clickable: true,
-          renderCustom: renderCustomPagination,
+          type: 'custom',
+          renderCustom,
         }}
+        // autoplay={{
+        //   delay: 10000,
+        //   disableOnInteraction: false,
+        // }}
+        speed={1500}
+        modules={[Pagination, Autoplay]}
+        className="mySwiper"
       >
-        {/* Create slide */}
         {animalsData.map(animal => (
           <SwiperSlide key={animal.index}>
-            <div className="hero__img">
-              <img src={animal.img} alt="reptiles" />
+            <div className="hero-content">
+              <img src={animal.img} alt={animal.text} />
               <CategoryType />
             </div>
           </SwiperSlide>
         ))}
+        <GlobalContainer>
+          <div
+            onClick={handlePaginationItemClick}
+            ref={paginationRef}
+            className="pagination-container"
+          ></div>
+        </GlobalContainer>
       </Swiper>
-
-      <AnimalsBarContainer>
-        <div className="swiper-pagination"></div>
-      </AnimalsBarContainer>
     </StyledHero>
   );
 };
