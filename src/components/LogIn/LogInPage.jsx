b70d7721ff289c059d231bc02e1d04ed86f40771
AuroraPolaire <sea.forest.mountains@gmail.com>
2023-09-06 14:06:25 +0300

    quickfix/added global container

--- src/components/LogIn/LogInPage.jsx
+++ src/components/LogIn/LogInPage.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { GlobalContainer } from '../../global/GlobalContainer';
 import useWindowSize from '../../hooks/useWindowSize';
 import LogInBox from './LogInBox';
 import { StyledLoginPage } from './LogInPage.styled';
@@ -7,10 +8,22 @@ import NewClient from './NewClient';
 const LogInPage = () => {
   const { screen } = useWindowSize();
   return (
-    <StyledLoginPage>
-      <LogInBox />
-      {screen !== 'mobile' && <NewClient />}
-    </StyledLoginPage>
+    <>
+      {screen !== 'mobile' && (
+        <GlobalContainer>
+          <StyledLoginPage>
+            <LogInBox />
+            <NewClient />
+          </StyledLoginPage>
+        </GlobalContainer>
+      )}
+
+      {screen === 'mobile' && (
+        <StyledLoginPage>
+          <LogInBox />
+        </StyledLoginPage>
+      )}
+    </>
   );
 };
 
--- src/components/LogIn/LogInPage.styled.jsx
+++ src/components/LogIn/LogInPage.styled.jsx
@@ -4,11 +4,11 @@ export const StyledLoginPage = styled.div`
   @media screen and (min-width: 834px) {
     gap: 16px;
     display: flex;
-    margin: 24px 40px;
+    margin: 24px 0;
   }
 
   @media screen and (min-width: 1440px) {
     gap: 24px;
-    margin: 24px 150px;
+    margin: 24px 0;
   }
 `;
